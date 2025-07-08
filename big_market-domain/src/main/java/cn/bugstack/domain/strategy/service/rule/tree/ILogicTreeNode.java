87147ae6eec3d8db3d2bae53986062b98e2b9feb
zxy <zxy0613zxy@outlook.com>
2025-07-08 20:25:05 +0800

    抽奖规则树模型结构设计

---
+++ big_market-domain/src/main/java/cn/bugstack/domain/strategy/service/rule/tree/ILogicTreeNode.java
@@ -0,0 +1,13 @@
+package cn.bugstack.domain.strategy.service.rule.tree;
+
+import cn.bugstack.domain.strategy.service.rule.tree.factory.DefaultTreeFactory;
+
+/**
+ * @description 规则树接口
+ * @create 2024-01-27 11:14
+ */
+public interface ILogicTreeNode {
+
+    DefaultTreeFactory.TreeActionEntity logic(String userId, Long strategyId, Integer awardId);
+
+}
---
+++ big_market-domain/src/main/java/cn/bugstack/domain/strategy/service/rule/tree/factory/engine/IDecisionTreeEngine.java
@@ -0,0 +1,13 @@
+package cn.bugstack.domain.strategy.service.rule.tree.factory.engine;
+
+import cn.bugstack.domain.strategy.service.rule.tree.factory.DefaultTreeFactory;
+
+/**
+ * @description 规则树组合接口
+ * @create 2024-01-27 11:33
+ */
+public interface IDecisionTreeEngine {
+
+    DefaultTreeFactory.StrategyAwardData process(String userId, Long strategyId, Integer awardId);
+
+}
